5dd3df8ca2838488d79189469404e9a259493575
Monir <mrmonir0558@gmail.com>
2025-02-21 10:07:48 +0600

    project setup and Post task method almost done

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhc73.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+async function run() {
+  try {
+      const taskManagement = client.db('taskManagement');
+      const taskCollection = taskManagement.collection('allTasks');
+      const userCollection = taskManagement.collection('allUsers');
+
+      // Get Users
+      app.get('/users', async (req, res) => {
+          const findUsers = userCollection.find({});
+          const result = await findUsers.toArray();
+          res.send(result);
+      });
+
+      // Post User
+      app.post('/user', async (req, res) => {
+          console.log(req.body);
+          const userInfo = req.body;
+          const id = { userID: userInfo.userID };
+          const existedUser = await userCollection.findOne(id);
+
+          if (existedUser) {
+              return res.send({ message: 'User already exists', insertedId: null });
+          }
+
+          const insertResult = await userCollection.insertOne(userInfo);
+          res.send(insertResult);
+      });
+
+      // Get All Tasks
+      app.get('/tasks', async (req, res) => {
+          const uid = req.query.uid;
+          const query = { UserID: uid };
+          const findTasks = taskCollection.find(query);
+          const result = await findTasks.toArray();
+          res.send(result);
+      });
+
+      // Post Task
+      app.post('/tasks', async (req, res) => {
+          const taskData = req.body;
+          const insertResult = await taskCollection.insertOne(taskData);
+          res.send(insertResult);
+      });
+
+      // Send a ping to confirm a successful connection
+      await client.db('admin').command({ ping: 1 });
+      console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+app.get('/', (req, res) => {
+  res.send('Hello from Task Management Server....')
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
