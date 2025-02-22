433a9a9f3b671b2e9d2c0f3890d66e351873390d
Monir <mrmonir0558@gmail.com>
2025-02-22 11:17:38 +0600

    solve the cors related problem

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: ['http://localhost:5173/', 'https://task-management-client-e78f5.web.app', 'https://task-management-client-e78f5.firebaseapp.com'],
+        credentials: true
+    })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhc73.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
